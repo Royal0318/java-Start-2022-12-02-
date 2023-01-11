8c644bec21f1379d872ebbaced045dc40f341020
도현우 <118295093+Royal0318@users.noreply.github.com>
2023-01-11 18:15:41 +0900

    for문과 메소드를 이용한 문제 (2023-01-11) for문과 메소드를 이용하여 예전에 풀었던 배열문제를 풀고있다

--- January/src/Januaryeleventh.java
+++ January/src/Januaryeleventh.java
@@ -17,54 +17,59 @@ public class Januaryeleventh {
 
     public static int MenuSelect(int Number, int ArrayNumber) {
         Scanner sc = new Scanner(System.in);
-
         int[] arr = new int[ArrayNumber];
 
-        for (int i = 1; i <= ArrayNumber; i++) {
-            System.out.println("1.더하기  2.빼기  3.특정 값 호출");
-            int Select = sc.nextInt();
+        System.out.println("1.더하기  2.빼기  3.특정 값 호출");
+        int Select = sc.nextInt();
 
-            if (Select == 1) {
-                return Plus(arr, Number, ArrayNumber, i);
-            }
-            if (Select == 2) {
-                return Minus(arr, Number, ArrayNumber, i);
-            }
-            if (Select == 3) {
-                return NumberCall(arr, Number, ArrayNumber, i);
-            }
+        if (Select == 1) {
+            return Plus(arr, Number, ArrayNumber,Select);
+        }
+        if (Select == 2) {
+            return Minus(arr, Number, ArrayNumber,Select);
+        }
+        if (Select == 3) {
+            return NumberCall(arr, Number, ArrayNumber,Select);
         }
         return 0;
     }
 
-    public static int Plus(int[] arr, int Number, int ArrayNumber, int i) {
+    public static int Plus(int[] arr, int Number, int ArrayNumber,int Select) {
         Scanner sc = new Scanner(System.in);
         System.out.println("숫자 입력");
         Number = sc.nextInt();
-        arr[i] = arr[i - 1] + Number;
-        return Calculation(arr, Number, ArrayNumber, i);
+        return Cal(arr,Number,ArrayNumber,Select);
     }
 
-    public static int Minus(int[] arr, int Number, int ArrayNumber, int i) {
+    public static int Minus(int[] arr, int Number, int ArrayNumber,int Select) {
         Scanner sc = new Scanner(System.in);
         System.out.println("숫자 입력");
         Number = sc.nextInt();
-        arr[i] = arr[i - 1] - Number;
-        return Calculation(arr, Number, ArrayNumber, i);
+        return Cal(arr,Number,ArrayNumber,Select);
     }
 
-    public static int NumberCall(int[] arr, int Number, int ArrayNumber, int i) {
+    public static int NumberCall(int[] arr, int Number, int ArrayNumber,int Select) {
         Scanner sc = new Scanner(System.in);
         System.out.println("특정 값 입력");
         Number = sc.nextInt();
-        arr[i] = Number;
-        return Calculation(arr, Number, ArrayNumber, i);
+        return Cal(arr,Number,ArrayNumber,Select);
     }
 
-    public static int Calculation(int[] arr, int Number, int ArrayNumber, int i) {
-        for (int j = 0; j <= i; j++) {
-            System.out.println("" + (j + 1) + "번째 숫자 : " + arr[j] + "");
+    public static int Cal(int[]arr,int Number,int ArrayNumber,int Select) {
+        for (int i = 1; i <= ArrayNumber; i++) {
+
+            if (Select == 1) {
+                arr[i] = arr[i - 1] + Number;
+            } else if (Select == 2) {
+                arr[i] = arr[i - 1] - Number;
+            } else if (Select == 3) {
+                arr[i] = Number;
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.println("" + (j + 1) + "번째 숫자 : " + arr[j] + "");
+            }
+            MenuSelect(Number,ArrayNumber);
         }
-        return MenuSelect(Number, ArrayNumber);
+        return 0;
     }
 }
