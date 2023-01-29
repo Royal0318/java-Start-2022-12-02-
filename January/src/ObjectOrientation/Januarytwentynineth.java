4e7dad93f8957f063dfd86e0b93537935ce69da7
도현우 <118295093+Royal0318@users.noreply.github.com>
2023-01-29 15:31:34 +0900

    객체지향 기초 : 게임기획서 소개하기 (2023,01,27)

--- January/src/ObjectOrientation/Januarytwentynineth.java
+++ January/src/ObjectOrientation/Januarytwentynineth.java
@@ -4,9 +4,11 @@ import java.util.Scanner;
 
 public class Januarytwentynineth {
     public static void main(String[] args) {
+        //게임리스트에서 종료버튼을 누를시 출력되어 시스템종료
         System.out.println(""+GameNameMake()+"");
     }
     public static String GameNameMake () {
+        //게임제목 정하는 메소드
         Scanner sc = new Scanner(System.in);
         System.out.println("4번째 게임의 제목을 정해주세요");
         String Game4 = sc.next();
@@ -15,12 +17,13 @@ public class Januarytwentynineth {
         return LookGameExplanation(Game4,Game5);
     }
     public static String LookGameExplanation(String Game4,String Game5) {
+        //게임 제목 리스트중에서 기획서내용을 보기위한 선택메소드
         Scanner sc = new Scanner(System.in);
         Januarytwentynineth2 TotalGame = new Januarytwentynineth2("Card Game", "빨리 보스잡기 게임", "격투게임", ""+Game4+"", ""+Game5+"", "", "", "", "", "", "", "");
 
         while (true) {
             TotalGame.GameMainList();
-            int GameListInput = sc.nextInt(); //기획서를 보고싶은 게임번호
+            int GameListInput = sc.nextInt();
 
             if (GameListInput == 1 || GameListInput == 2 || GameListInput == 3 || GameListInput == 4 || GameListInput == 5) {
                 return ListDescription(GameListInput,Game4,Game5);
@@ -32,6 +35,7 @@ public class Januarytwentynineth {
         }
     }
     public static String ListDescription (int GameListInput,String Game4,String Game5) {
+        //게임기획서를 보기위해 게임을 선택하면 기획서가 출력됨
         Januarytwentynineth2 TotalGame = new Januarytwentynineth2("Card Game", "빨리 보스잡기 게임", "격투게임", ""+Game4+"", ""+Game5+"", "", "", "", "", "", "", "");
 
         if (GameListInput == 1) {
