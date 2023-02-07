e5275e0ef59ad09cabf5ba6484659bbd397ff30a
도현우 <118295093+Royal0318@users.noreply.github.com>
2023-02-07 20:52:37 +0900

    객체지향과 array리스트를 이용한 자기소개 (2023년2월7일)

---
+++ January/src/test.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class test {
+    public static void main(String[] args) {
+         test2 han = new test2("홍길동","ab","경기도");
+        test2 kim = new test2("김민지","b","충청도");
+        test2 lee = new test2("최현우","a","부산");
+
+        ArrayList<test2> test = new ArrayList<>();
+        test.add(han);
+        test.add(kim);
+        test.add(lee);
+
+        for (test2 arr2 : test) {
+            arr2.dawd();
+        }
+    }
+}
---
+++ January/src/test2.java
@@ -0,0 +1,16 @@
+public class test2 {
+    String Name;
+    String Bolood;
+    String address;
+
+    test2 (String Name,String Bolood,String address) {
+        this.Name = Name;
+        this.Bolood = Bolood;
+        this.address = address;
+    }
+    void dawd () {
+        System.out.println("이름 : "+Name+"");
+        System.out.println("혈액형 : "+Bolood+"");
+        System.out.println("주소 : "+address+"");
+    }
+}
