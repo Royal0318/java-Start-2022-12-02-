c9f4cba4e53508340cb7f8a9e43ad09a096346a6
도현우 <118295093+Royal0318@users.noreply.github.com>
2023-02-14 16:25:40 +0900

    반복문 문제풀이 (반복문 종합문제) 3번풀이(객제지향 이용)(2023년 2월14일 ~)

--- forwhile/src/forwhile19.java
+++ forwhile/src/forwhile19.java
@@ -1,39 +1,15 @@
-import java.util.Scanner;
 
 public class forwhile19 {
     public static void main(String[] args) {
-        int Foodbuymoney = 0;
-        int Foodbuycheck = 0;
-        int Calculatorsum = 0;
-        int Calmenuoutcheck = 0;
-        forwhile20 FoodMet = new forwhile20(Foodbuymoney,Foodbuycheck,Calculatorsum,Calmenuoutcheck);
+        //main값에 변수를 보관한다
+        int Foodbuymoney = 0; //음식을 구매한 값을 저장
+        int Foodbuycheck = 0; //식당 재입장 여부 확인
+        int Calculatorsum = 0; //계산기 마지막값 기억
+        int Calmenuoutcheck = 0; //계산기 재진입 여부 확인
+        forwhile20 FoodMet = new forwhile20(Foodbuymoney, Foodbuycheck, Calculatorsum, Calmenuoutcheck);
         forwhile20 FoodMet2 = new forwhile20();
-        //예비 생성자
-        Selectmenu(Foodbuymoney,Calmenuoutcheck,FoodMet);
-
-    }
-     static void Selectmenu(int Foodbuymoney, int Calmenuoutcheck, forwhile20 FoodMet) {
-        Scanner sc = new Scanner(System.in);
-        while (true) {
-            System.out.println("[1.구구단 2.음식점 3.계산기 4.종료]");
-            int Menuchoice = sc.nextInt();
-
-            if (Menuchoice == 1) {
-                FoodMet.Gugudan();
-            } else if (Menuchoice == 2) {
-                FoodMet.Restaurant();
-            } else if (Menuchoice == 3) {
-                if (Calmenuoutcheck == 1) {
-                    FoodMet.AgainCalculator();
-                } else {
-                    FoodMet.Calculator();
-                }
-            } else if (Menuchoice == 4) {
-                System.out.println("시스템을 종료합니다 \n총 누적금액 : "+Foodbuymoney+"원");
-                break;
-            } else {
-                System.out.println("잘못된 입력입니다");
-            }
-        }
+        //예비생성자
+        FoodMet.Selectmenu(FoodMet);
+        //메인값을 불러옴
     }
 }
--- forwhile/src/forwhile20.java
+++ forwhile/src/forwhile20.java
@@ -16,7 +16,33 @@ public class forwhile20 {
         this.Calmenutoutcheck = Calmenuoutcheck;
     }
 
+    public void Selectmenu (forwhile20 FoodMet) {
+        Scanner sc = new Scanner(System.in);
+        //메인 선택 메소드
+        while (true) {
+            System.out.println("[1.구구단 2.음식점 3.계산기 4.종료]");
+            int Menuchoice = sc.nextInt();
+
+            if (Menuchoice == 1) {
+                FoodMet.Gugudan();
+            } else if (Menuchoice == 2) {
+                FoodMet.Restaurant(FoodMet);
+            } else if (Menuchoice == 3) {
+                if (Calmenutoutcheck == 1) {
+                    FoodMet.AgainCalculator(FoodMet);
+                } else {
+                    FoodMet.Calculator(FoodMet);
+                }
+            } else if (Menuchoice == 4) {
+                System.out.println("시스템을 종료합니다 \n총 누적금액 : "+Foodbuymoney+"원");
+                System.exit(1);
+            } else {
+                System.out.println("잘못된 입력입니다");
+            }
+        }
+    }
     void Gugudan() {
+        //1 ~ 9 단까지 출력 출력후 메인선택 메소드로 리턴
         for (int i = 0; i < 9; i++) {
             System.out.println("================" + (i + 1) + "단================");
             for (int j = 0; j < 9; j++) {
@@ -25,26 +51,27 @@ public class forwhile20 {
         }
     }
 
-     void Restaurant() {
+     void Restaurant(forwhile20 FoodMet) {
+        //식당 메소드 음식을 주문하면 가격을 Ordercomplete메소드로 리턴시켜 중복을 제거
         Scanner sc = new Scanner(System.in);
         System.out.println("1.볶음밥[8000원] 2.짜장면[7000원] 3.탕수육 4.돌아가기");
         int Foodchoice = sc.nextInt();
 
         if (Foodchoice == 1) {
-            Ordercomplete(8000);
+            Ordercomplete(8000,FoodMet);
         } else if (Foodchoice == 2) {
-            Ordercomplete(7000);
+            Ordercomplete(7000,FoodMet);
         } else if (Foodchoice == 3) {
             while (true) {
                 System.out.println("1.Size 대[25000원] 2.Size 중[20000원] 3.Size 소[17000원]");
                 int Splash = sc.nextInt();
 
                 if (Splash == 1) {
-                    Ordercomplete(25000);
+                    Ordercomplete(25000,FoodMet);
                 } else if (Splash == 2) {
-                    Ordercomplete(20000);
+                    Ordercomplete(20000,FoodMet);
                 } else if (Splash == 3) {
-                    Ordercomplete(17000);
+                    Ordercomplete(17000,FoodMet);
                 } else {
                     System.out.println("잘못된 입력입니다");
                 }
@@ -55,21 +82,23 @@ public class forwhile20 {
                 Foodbuycheck += 1;
             }
             System.out.println("메인메뉴로 돌아갑니다");
-            //메인메뉴로 리턴하는방법?
+            Selectmenu(FoodMet);
         } else {
             System.out.println("잘못된 입력입니다");
-            Restaurant();
+            Restaurant(FoodMet);
         }
     }
-    void Ordercomplete(int Buyfoodprice) {
+    void Ordercomplete(int Buyfoodprice,forwhile20 FoodMet) {
+        //구매값을 리턴받아 주문완료시킨다 단 재입장하지 않았을경우 값을 저장 재입장 구매인경우 값을 저장하지 않음
         if (Foodbuycheck == 0) {
             Foodbuymoney += Buyfoodprice;
         }
         System.out.println("주문이 완료되었습니다 누적금액 : " + Foodbuymoney + "원");
-        Restaurant();
+        Restaurant(FoodMet);
     }
 
-    void Calculator() {
+    void Calculator(forwhile20 FoodMet) {
+        //계산기 메인메소드 계산을 완료하면 입력한 마지막값을 저장하며 부호와 값을 리턴시킨다
         Scanner sc = new Scanner(System.in);
         System.out.println("<<<1.더하기  2.빼기  3.돌아가기>>>");
         int Calmethodchoice = sc.nextInt();
@@ -81,27 +110,28 @@ public class forwhile20 {
 
             if (Calmethodchoice == 1) {
                 Calculatorsum = (Number + Number2);
-                Calcomplete(Number, Number2, "+");
+                Calcomplete(Number, Number2, "+",FoodMet);
             } else {
                 if (Number < Number2) {
                     System.out.println("두 번째 값이 더 클 수 없습니다!");
-                    Calculator();
+                    Calculator(FoodMet);
                 } else {
                     Calculatorsum = (Number - Number2);
-                    Calcomplete(Number, Number2, "-");
+                    Calcomplete(Number, Number2, "-",FoodMet);
                 }
             }
         } else if (Calmethodchoice == 3) {
             Calmenutoutcheck += 1;
             System.out.println("메인메뉴로 돌아갑니다 \n안내 : 마지막으로 계산한 값 : " + Calculatorsum + "");
-            //메인메뉴로 리턴하는방법?
+            Selectmenu(FoodMet);
         } else {
             System.out.println("잘못된 입력입니다");
-            Calculator();
+            Calculator(FoodMet);
         }
     }
 
-    void AgainCalculator() {
+    void AgainCalculator(forwhile20 FoodMet) {
+        //다시 계산기 메소드에 진입할경우 2개의 수를 받지않고 기존 마지막값에서 2번째값을 계산한다
         Scanner sc = new Scanner(System.in);
         System.out.println("<<<1.더하기  2.빼기  3.돌아가기>>>");
         int Calmethodchoice = sc.nextInt();
@@ -111,39 +141,43 @@ public class forwhile20 {
             int Number2 = sc.nextInt();
 
             if (Calmethodchoice == 1) {
-                Againcalcomplete(Number2, "+");
+                Againcalcomplete(Number2, "+",FoodMet);
             } else {
                 if (Calculatorsum < Number2) {
                     System.out.println("두 번째 값이 더 큽니다");
-                    AgainCalculator();
+                    AgainCalculator(FoodMet);
                 } else {
-                    Againcalcomplete(Number2, "-");
+                    Againcalcomplete(Number2, "-",FoodMet);
                 }
             }
         } else if (Calmethodchoice == 3) {
             System.out.println("메인메뉴로 돌아갑니다 \n안내 : 마지막으로 계산한 값 : " + Calculatorsum + "");
-            //메인메뉴로 리턴하는방법?
+            Selectmenu(FoodMet);
         } else {
             System.out.println("잘못된 입력입니다");
-            AgainCalculator();
+            AgainCalculator(FoodMet);
         }
     }
 
-    void Calcomplete(int Number, int Number2, String Sign) {
+    void Calcomplete(int Number, int Number2, String Sign,forwhile20 FoodMet) {
+        //재입장을 하지않았을때 계속 이 메소드를 사용한다
         if (Sign.equals("+")) {
             System.out.println("" + Number + " + " + Number2 + " = " + (Number + Number2) + "");
         } else if (Sign.equals("-")) {
             System.out.println("" + Number + " - " + Number2 + " = " + (Number - Number2) + "");
         }
-        Calculator();
+        Calculator(FoodMet);
     }
 
-    void Againcalcomplete(int Number2, String Sign) {
+    void Againcalcomplete(int Number2, String Sign,forwhile20 FoodMet) {
+        //재입장 계산후 리턴받는 메소드
         if (Sign.equals("+")) {
             System.out.println("" + Calculatorsum + " + " + Number2 + " = " + (Calculatorsum + Number2) + "");
+            Calculatorsum += Number2;
         } else if (Sign.equals("-")) {
             System.out.println("" + Calculatorsum + " - " + Number2 + " = " + (Calculatorsum - Number2) + "");
+            Calculatorsum -= Number2;
         }
-        AgainCalculator();
+        AgainCalculator(FoodMet);
     }
 }
