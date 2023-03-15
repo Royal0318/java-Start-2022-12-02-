7fad818d8c0c3dd7a5af0900f1990baca2d6171d
도현우 <edorop@naver.com>
2023-03-15 16:24:10 +0900

    알고리즘 : 백준 (4673번) 문제제목 : 셀프넘버

--- Baekjoon/src/Question4673.java
+++ Baekjoon/src/Question4673.java
@@ -7,7 +7,6 @@ public class Question4673 {
                 String str = "" + i + "";
                 int length = str.length();
                 int sum = 0;
-
                 //각각의 자리값을 length에 저장하며각자릿수의 합을sum변수에 저장
                 for (int j = 0; j < length; j++) {
                     sum += str.charAt(j) - '0';
