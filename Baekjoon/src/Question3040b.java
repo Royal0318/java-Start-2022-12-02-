78d33091391d89746d7cf35887950d0118eafdc3
도현우 <edorop@naver.com>
2023-03-23 14:06:24 +0900

    알고리즘 : 백준 (3040번) 문제제목 : 백설공주와 일곱난쟁이 풀이방법3번

---
+++ Baekjoon/src/Question3040b.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Question3040b {
+    static int[] arr = new int[9];
+    static int sum = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        for (int i = 0;i < 9;i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+            sum += arr[i];
+        }
+        f(0,0);
+    }
+    static void f (int one,int two) {
+        if (two >= 8) {
+            if (sum - (arr[one] + arr[two]) == 100) {
+                arr[one] = 0;
+                arr[two] = 0;
+                for (int i = 0; i < 9;i++) {
+                    if (arr[i] != 0) {
+                        System.out.println(arr[i]);
+                    }
+                }
+                return;
+            } else {
+                one += 1;
+                two = 0;
+            }
+        }
+        f(one, two+1);
+    }
+}
