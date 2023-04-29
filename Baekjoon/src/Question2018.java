07a035dac83054f7a21f9650faedc09aea2c1bc6
도현우 <edorop@naver.com>
2023-04-29 16:16:48 +0900

    알고리즘 제목 : (구현) 통계학

--- Baekjoon/src/Question2018.java
+++ Baekjoon/src/Question2018.java
@@ -10,7 +10,7 @@ public class Question2018 {
         int end = 1; //끝지점
         int sum = 1; //수의합
         int result = 1; //정답
-
+//배열 아닌경우
        while (end != num) { //end가 num와 같아지면 break로 자동탈출되서 정답도출
            if (sum == num) { //같으면 end조정
                end += 1;
