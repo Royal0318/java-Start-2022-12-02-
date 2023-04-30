af5a74fdfb0067cadab169e08440143524ffe763
도현우 <edorop@naver.com>
2023-04-30 18:06:54 +0900

    알고리즘 제목 : (구현) 펠린드롬인지 확인하기

---
+++ Baekjoon/src/Question10988.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Question10988 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+
+        for (int i = 0; i < str.length();i++) {
+            if (str.charAt(i) != str.charAt(str.length() - 1 - i)) {
+                System.out.println(0);
+                return;
+            }
+        }
+        System.out.println(1);
+    }
+}
