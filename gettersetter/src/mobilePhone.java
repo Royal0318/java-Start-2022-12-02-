a88ad1fd2a17f64d2bd4b58f4602e7fe717c177f
도현우 <edorop@naver.com>
2023-05-25 20:43:02 +0900

    객체지향 : 인터페이스 : 객체지향 인터페이스와 Timetask문법을 이용한 휴대폰 타이머기능

--- gettersetter/src/mobilePhone.java
+++ gettersetter/src/mobilePhone.java
@@ -1,6 +1,6 @@
 
-public interface mobilePhone  { //추상클래스 핸드폰 인터페이스는 class필요없다
-    void on ();
+public interface mobilePhone { //인터페이스 : 인터페이스는 abstract가 미리 적용되어있어 굳이 적을필요 X
+    void on (); //추상 메소드
     void off ();
     void display ();
     void timeOver ();
--- gettersetter/src/phoneMain.java
+++ gettersetter/src/phoneMain.java
@@ -1,11 +1,18 @@
 import java.util.Scanner;
-
-public class phoneMain { //컨트롤러
+/*
+1.Phone이라는 프로젝트를 생성하고 mobilePhone이라는 이름의 인터페이스를 만든다
+2.인터페이스 모바일 폰은 껏다켯다가 가능합니다 그래서 추상메소드로 on,off를 만든다
+3.휴대푠이 켜지고 텍스트를 입력받아 입력후 화면에 출력을 하고 종료
+4.만약에 입력을 하지않고 30초가 지났을경우 프로그램 종료
+5.인터페이스에 static상수로 Timeout을 만들고 30초에 맞출것
+6.텍스트를 입력받아 화면을 출력할 수 있는 추상메소드 display도 만들것
+ */
+public class phoneMain {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         mobilePhone phone = new Phone();
 
-        phone.on(); //처음 휴대폰 켜짐
+        phone.on();
         while (true) {
             System.out.println("1.휴대폰종료 2.텍스트 입력");
 
