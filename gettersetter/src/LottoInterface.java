6d5dcb3d5607cbbebcc92910865445396511f841
도현우 <edorop@naver.com>
2023-05-31 18:05:43 +0900

    제목 : 객체지향을 이용하여 로또프로그램을 제작

---
+++ gettersetter/src/LottoInterface.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public interface LottoInterface  {
+    Scanner sc = new Scanner(System.in);
+    int lottoMaxNumber = 6; //최대 6까지 입력가능하다
+
+
+    void inputMyMoney ();
+
+
+    void buyLotto();
+
+    void inputMyInputNumber();
+}
---
+++ gettersetter/src/lottoMain.java
@@ -0,0 +1,9 @@
+public class lottoMain {
+    public static void main(String[] args) {
+        LottoInterface lottoInterface = new LotooProgram();
+        lottoInterface.inputMyMoney();
+        lottoInterface.buyLotto();
+        lottoInterface.inputMyInputNumber();
+
+    }
+}
